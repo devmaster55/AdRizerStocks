d7161754a76327a53e39f5597c8c6b0f169847c5
alex-s85 <worldmaster5558@hotmail.com>
2021-03-04 15:27:15 +0100

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+import StocksPage from './Pages/StocksPage';
+
+const App = () => {
+  return (
+    <div className="App">
+      <StocksPage />
+    </div>
+  );
+}
+
+export default App;
