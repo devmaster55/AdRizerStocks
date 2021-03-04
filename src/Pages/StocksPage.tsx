d31be8610d6dbdd7d34b9f242d3335a6de17b3c0
alex-s85 <worldmaster5558@hotmail.com>
2021-03-04 16:16:32 +0100

    place AgGridReact

--- src/Pages/StocksPage.tsx
+++ src/Pages/StocksPage.tsx
@@ -6,18 +6,23 @@ import 'ag-grid-community/dist/styles/ag-theme-alpine.css';
 
 const StocksPage = () => {
   const [rowData, setRowData] = useState([
-    { make: "Toyota", model: "Celica", price: 35000 },
-    { make: "Ford", model: "Mondeo", price: 32000 },
-    { make: "Porsche", model: "Boxter", price: 72000 }
+    { timestamp: "2021-03-03 20:00:00", total_volumn: 3611, min_price: 121.9400, max_price: 122.0000, opening_price: 121.9400, closing_price: 122.0000 },
+    { timestamp: "2021-03-03 19:50:00", total_volumn: 855, min_price: 121.9400, max_price: 122.0000, opening_price: 121.9400, closing_price: 122.0000 },
+    { timestamp: "2021-03-03 19:40:00", total_volumn: 216, min_price: 121.9400, max_price: 122.0000, opening_price: 121.9400, closing_price: 122.0000 },
+    { timestamp: "2021-03-03 19:25:00", total_volumn: 386, min_price: 121.9400, max_price: 122.0000, opening_price: 121.9400, closing_price: 122.0000 },
+    { timestamp: "2021-03-03 19:05:00", total_volumn: 312, min_price: 121.9400, max_price: 122.0000, opening_price: 121.9400, closing_price: 122.0000 },
   ]);
 
   return (
-    <div className="ag-theme-alpine" style={{ height: 400, width: 600 }}>
+    <div className="ag-theme-alpine" style={{ height: 600, width: 1200 }}>
       <AgGridReact
           rowData={rowData}>
-          <AgGridColumn field="make"></AgGridColumn>
-          <AgGridColumn field="model"></AgGridColumn>
-          <AgGridColumn field="price"></AgGridColumn>
+          <AgGridColumn headerName="Timestamp" field="timestamp"></AgGridColumn>
+          <AgGridColumn headerName="Total Volume" field="total_volumn"></AgGridColumn>
+          <AgGridColumn headerName="Min. Price" field="min_price"></AgGridColumn>
+          <AgGridColumn headerName="Max. Price" field="max_price"></AgGridColumn>
+          <AgGridColumn headerName="Opening Price" field="opening_price"></AgGridColumn>
+          <AgGridColumn headerName="Closing Price" field="closing_price"></AgGridColumn>
       </AgGridReact>
     </div>
   );
